81a70cd3a3b05e81c8bfdfd4d6ebc30a2c6d0bab
Juan Correa <Juan.Correa@endava.com>
2020-10-17 19:05:25 -0500

    lesson 3, lambdas examples and secondAssigment

--- src/main/java/lessons/basics/CollectionTypes.java
+++ src/main/java/lessons/basics/CollectionTypes.java
@@ -17,7 +17,7 @@ public class CollectionTypes {
         Set<Integer> set1 = new HashSet<>();
         set1.add(1);
 
-        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> list1 = Arrays.asList(1, 2, 2, 3, 4, 5, 6);
         Set<Integer> set2 = new HashSet<>(list1);
 
         Collections.addAll(set2, 3, 4, 5, 6);
