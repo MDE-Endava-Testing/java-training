5eabe427c2ce8f16342aa565475fda86fd568015
Juan Campos Garzon <juan.campos@endava.com>
2020-10-22 11:13:35 -0500

    adding test for interfaces

---
+++ src/main/java/homeworks/juanpa/firstAssigment/interfaces/ImplementInterfaces.java
@@ -0,0 +1,12 @@
+package homeworks.juanpa.firstAssigment.interfaces;
+
+public class ImplementInterfaces implements InterfaceTest{
+
+    /**
+     * Overriding the method of interface
+     */
+    @Override
+    public void test1() {
+        System.out.println("this is an example");
+    }
+}
