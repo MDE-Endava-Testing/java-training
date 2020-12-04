211c4afcb07cb0b75a9bdd591d88c340ee723006
cristian <cristianlopera24@gmail.com>
2020-12-04 13:33:44 -0500

    added some other things in the sixth assignment

--- src/main/java/homeworks/christian/sixthAssignment/Main.java
+++ src/main/java/homeworks/christian/sixthAssignment/Main.java
@@ -3,7 +3,12 @@ package homeworks.christian.sixthAssignment;
 public class Main {
 
     public static void main(String[] args){
+        Calculator calculate = new Calculator();
+        String expresion1 = "5 + 2 - 3 * 7 + 2 / 3";
+        String expresion2 = "10 / 2 * 3 - 3 / 3 * 5";
+        String expresion3 = "9 * 9 - 1 * 7 / 8 + 30";
 
+        //Result = 10
+        System.out.println(calculate.calculate(expresion1));
     }
-
 }
