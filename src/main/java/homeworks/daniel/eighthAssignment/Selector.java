831b09b51141b100bf32260aa792b1c79496c2b8
Daniel Gutierrez Cardona <Daniel.Gutierrez@endava.com>
2020-12-10 15:47:50 -0500

    new method for predicates

--- src/main/java/homeworks/daniel/eighthAssignment/Selector.java
+++ src/main/java/homeworks/daniel/eighthAssignment/Selector.java
@@ -2,7 +2,11 @@ package homeworks.daniel.eighthAssignment;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Class with an util function used for selecting rows depending on the input condition
@@ -16,5 +20,14 @@ public class Selector {
         driver.findElements(By.cssSelector("tr")).stream().filter(x->{
             return filterCondition.test(x.getText());
         }).forEach(z->z.findElement(By.cssSelector("input")).click());
+    }
+
+    /**
+     * creates a predicate for each keyword
+     * @param keywords list of keywords
+     * @return Map containing the keywords and its respective predicate
+     */
+    public static Map<String,Predicate<String>> getPredicates(List<String> keywords){
+        return keywords.stream().collect(Collectors.toMap(str->str,str->x->x.contains(str)));
     }
 }
--- src/main/java/homeworks/daniel/eighthAssignment/TestFilter.java
+++ src/main/java/homeworks/daniel/eighthAssignment/TestFilter.java
@@ -4,8 +4,14 @@ import homeworks.daniel.fourthAssignment.DriverFactory;
 import org.junit.Before;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
+import java.nio.channels.SeekableByteChannel;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 /**
@@ -26,15 +32,12 @@ public class TestFilter {
     }
 
     /**
-     * Testcase using different predicates to select different rows depending on its values
+     * Testcase using different predicates and combinations to select different rows depending on its values
      */
     @Test
     public void TestSelection(){
-        Predicate<String> femaleUK = str-> str.contains("Female") && str.contains("UK");
-        Predicate<String> female = str-> str.contains("Female") ;
-        Predicate<String> UK = str-> str.contains("UK");
-        Predicate<String> USA = str-> str.contains("USA");
-        Predicate<String> male = str-> str.contains("Male");
-        Selector.checkOption(female.or(USA),driver);
+        List<String> keywords= Arrays.asList("Female", "UK", "USA","Male");
+        Map<String,Predicate<String>> predicates = Selector.getPredicates(keywords);
+        Selector.checkOption(predicates.get("Female").and(predicates.get("UK")),driver);
     }
 }
